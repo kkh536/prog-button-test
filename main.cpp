741782895ceb7d13ecfc905f3facadfb2b9f9d8c
kkh536 <kkh536@york.ac.uk>
2025-09-22 13:55:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@ int main()
 {
     // Initialise the digital pin BUTTON1 (the blue button) as an input
     DigitalIn button(BUTTON1);
+    DigitalOut led(LED1);
 
     // Loop forever...
     while (true) {
         // Is the button being pressed?
         if (button == true) {
             printf("Button is up\n");
+            led = false;
         } else {
             printf("Button is down\n");
+            led = true;
         }    
         // Wait for 500ms
         thread_sleep_for(500);
